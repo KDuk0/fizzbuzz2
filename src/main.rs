c7244e9c1ac204145b7a9b45140fa85629bccdd3
KDuk0 <125793699+KDuk0@users.noreply.github.com>
2023-05-29 10:38:29 +0200

    Another variation of the Fizzbuzz game

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+fn fizzbuzz(n: u8) -> String {
+    if n % 15 == 0 {
+        "FizzBuzz".to_string()
+    }
+    else if n % 3 == 0 {
+        "Fizz".to_string()
+    }
+    else if n % 5 == 0 {
+        "Buzz".to_string()
+    } else {
+        n.to_string()
+    }
+}
+
+fn main() {
+    for n in 1..=100 {
+        println!("{}", fizzbuzz(n));
+    }
+}
